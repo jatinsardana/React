58924f7314b0c8a05735e1f007e318b00eb54c01
jatin sardana <jatinsardana2000@gmail.com>
2024-03-29 00:48:01 +0530

    another

--- 01ReactChai/src/Tailwind.jsx
+++ 01ReactChai/src/Tailwind.jsx
@@ -1,7 +1,11 @@
+import Card from "./components/Card"
+
 function Tailwind(){
     return(
         <>
             <h1 className="text-green-900 bg-white">Tailwind Test</h1>
+            <Card/>
+            <Card/>
         </>
     )
 }
